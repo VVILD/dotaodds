f9ba25dcf3b4a52b9ab656e71eaad3a0ea8d5ac2
sargun <sargungu@gmail.com>
2015-11-12 23:42:34 +0530

    urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-admin.autodiscover()
+from myapp.views import match_view
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'django_project.views.home', name='home'),
+    # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^match_view/', match_view, name='match_view'),
 )
